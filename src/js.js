9a6511132368185543f10cf6834f550700771c4d
viniciusborges17 <30813727+viniciusborges17@users.noreply.github.com>
2017-11-07 12:26:13 -0200

    Update js.js

--- src/js.js
+++ src/js.js
@@ -23,3 +23,79 @@ menu.addEventListener('click', function(){
 		ativo = true;
 	}
 });
+
+document.getElementById("cadastro").onsubmit=function(event){
+	var nomeCampo = document.getElementById("nome");
+	var sexoCampo = document.getElementById("sexo");
+	var emailCampo = document.getElementById("email");
+	
+	var telCampo = document.getElementById("telefone");
+	var nascCampo = document.getElementById("nasc");
+	var raCampo = document.getElementById("ra");
+	var cursoCampo = document.getElementById("curso");
+	var estadoCampo = document.getElementById("estado");
+	var cidadeCampo = document.getElementById("cidade");
+	var bairroCampo = document.getElementById("bairro");
+	var enderecoCampo = document.getElementById("endereco");
+	var numeroCampo = document.getElementById("numero");
+	
+	if(nomeCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+	if (sexoCampo.value == "Selecione"){
+		alert("Campo obrigatório não preenchido");
+	    return false;
+
+	}
+	if (emailCampo.value == ""){
+		alert("Campo obrigatório não preenchido");
+	    return false;
+
+	}
+	if(telCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+	if(nascCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+	
+	if(raCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+
+	if(cursoCampo.value == "Selecione") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+	if(estadoCampo.value == "Selecione") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+	if(cidadeCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+	if(bairroCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+	if(enderecoCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+	
+	if(numeroCampo.value == "") {
+	    alert("Campo obrigatório não preenchido");
+	    return false;
+	}
+
+};
