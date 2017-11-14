fc48fe4f432f42f81b01a9f9d0a4b7b69d70d04b
vcamilo <vic12hugo@hotmail.com>
2017-11-14 21:40:29 -0200

    COrrigindo página de aluno

--- src/js.js
+++ src/js.js
@@ -1,13 +1,8 @@
 var menu = document.querySelectorAll('a')[1];
 var toggle = [];
 var ativo = true;
-var w = window.innerWidth;
 
 var ligaDesliga = document.querySelectorAll('.navbar li');
-
-if(window.innerWidth < 400){
-	console.log("menor");
-}
 
 menu.addEventListener('click', function(){
 	if(ativo == true){
@@ -38,78 +33,4 @@ document.getElementById("login").onsubmit=function(event){
 
 };
 
-document.getElementById("cadastro").onsubmit=function(event){
-	var nomeCampo = document.getElementById("nome");
-	var sexoCampo = document.getElementById("sexo");
-	var emailCampo = document.getElementById("email");
-	
-	var telCampo = document.getElementById("telefone");
-	var nascCampo = document.getElementById("nasc");
-	var raCampo = document.getElementById("ra");
-	var cursoCampo = document.getElementById("curso");
-	var estadoCampo = document.getElementById("estado");
-	var cidadeCampo = document.getElementById("cidade");
-	var bairroCampo = document.getElementById("bairro");
-	var enderecoCampo = document.getElementById("endereco");
-	var numeroCampo = document.getElementById("numero");
-	
-	if(nomeCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-	if (sexoCampo.value == "Selecione"){
-		alert("Campo obrigatório não preenchido");
-	    return false;
 
-	}
-	if (emailCampo.value == ""){
-		alert("Campo obrigatório não preenchido");
-	    return false;
-
-	}
-	if(telCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-	if(nascCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-	
-	if(raCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-
-	if(cursoCampo.value == "Selecione") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-	if(estadoCampo.value == "Selecione") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-	if(cidadeCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-	if(bairroCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-	if(enderecoCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-	
-	if(numeroCampo.value == "") {
-	    alert("Campo obrigatório não preenchido");
-	    return false;
-	}
-
-};
